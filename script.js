5e7096be9024c56c8b5e842776e6d22627167226
EmilBacklund <Emil.backlund@stud.noroff.no>
2022-01-06 13:53:22 +0100

    MA1 complete

---
+++ script.js
@@ -0,0 +1,24 @@
+var firstVar; // 1
+
+var name = "Emil"; // 2
+
+var myNumber = 9; // 3
+
+var division = 20 / 5; // 4
+
+typeof("frog"); // answer 5
+
+var animal = "frog";
+console.log(typeof animal); // also answer 5
+
+var orderHasShipped = true; // 6
+
+if (orderHasShipped === true) { // 7
+    console.log("true");
+} else  {
+console.log("false");
+}
+
+for (var count = 1; count <= 9; count++){ // 8
+    console.log(count);
+}
